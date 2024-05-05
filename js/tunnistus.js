dd56e4704926a5305b1755810d1365979473e420
sannituppu <t2tusa00@oamk.students.fi>
2024-05-05 18:51:46 +0300

    Linkkejä korjailtu

--- js/tunnistus.js
+++ js/tunnistus.js
@@ -2,26 +2,26 @@ const input = document.querySelector('input')
 const output = document.querySelector('output')
 
 const sanat = [
-    {name:"akku", image: "/pictures/sanasto/akku.jpg"},
-    {name:"diodi", image: "/pictures/sanasto/diodi.jpg"},
-    {name:"emolevy", image: "/pictures/sanasto/emolevy.jpg"},
-    {name:"fototransistori", image: "/pictures/sanasto/fototransistori.jpg"},
-    {name:"grafiikkakortti", image: "/pictures/sanasto/grafiikkakortti.jpg"},
-    {name:"hdmi-kaapeli", image: "/pictures/sanasto/hdmi-kaapeli.jpg"},
-    {name:"hiiri", image: "/pictures/sanasto/hiiri.jpg"},
-    {name:"ilmajäähdytin", image: "/pictures/sanasto/ilmajaahdytin.jpg"},
-    {name:"kiintolevy", image: "/pictures/sanasto/kiintolevy.jpg"},
-    {name:"kondensaattori", image: "/pictures/sanasto/kondensaattori.jpg"},
-    {name:"led", image: "/pictures/sanasto/led.jpg"},
-    {name:"mikropiiri", image: "/pictures/sanasto/mikropiiri.jpg"},
-    {name:"näppäimistö", image: "/pictures/sanasto/nappaimisto.jpg"},
-    {name:"näyttö", image: "/pictures/sanasto/naytto.jpg"},
-    {name:"prosessori", image: "/pictures/sanasto/prosessori.jpg"},
-    {name:"satunnaiskäyttömuisti", image: "/pictures/sanasto/satunnaiskayttomuisti.jpg"},
-    {name:"ssd", image: "/pictures/sanasto/ssd.jpg"},
-    {name:"transistori", image: "/pictures/sanasto/transistori.jpg"},
-    {name:"usb-kaapeli", image: "/pictures/sanasto/usb-kaapeli.jpg"},
-    {name:"vastus", image: "/pictures/sanasto/vastus.jpg"}
+    {name:"akku", image: "./pictures/sanasto/akku.jpg"},
+    {name:"diodi", image: "./pictures/sanasto/diodi.jpg"},
+    {name:"emolevy", image: "./pictures/sanasto/emolevy.jpg"},
+    {name:"fototransistori", image: "./pictures/sanasto/fototransistori.jpg"},
+    {name:"grafiikkakortti", image: "./pictures/sanasto/grafiikkakortti.jpg"},
+    {name:"hdmi-kaapeli", image: "./pictures/sanasto/hdmi-kaapeli.jpg"},
+    {name:"hiiri", image: "./pictures/sanasto/hiiri.jpg"},
+    {name:"ilmajäähdytin", image: "./pictures/sanasto/ilmajaahdytin.jpg"},
+    {name:"kiintolevy", image: "./pictures/sanasto/kiintolevy.jpg"},
+    {name:"kondensaattori", image: "./pictures/sanasto/kondensaattori.jpg"},
+    {name:"led", image: "./pictures/sanasto/led.jpg"},
+    {name:"mikropiiri", image: "./pictures/sanasto/mikropiiri.jpg"},
+    {name:"näppäimistö", image: "./pictures/sanasto/nappaimisto.jpg"},
+    {name:"näyttö", image: "./pictures/sanasto/naytto.jpg"},
+    {name:"prosessori", image: "./pictures/sanasto/prosessori.jpg"},
+    {name:"satunnaiskäyttömuisti", image: "./pictures/sanasto/satunnaiskayttomuisti.jpg"},
+    {name:"ssd", image: "./pictures/sanasto/ssd.jpg"},
+    {name:"transistori", image: "./pictures/sanasto/transistori.jpg"},
+    {name:"usb-kaapeli", image: "./pictures/sanasto/usb-kaapeli.jpg"},
+    {name:"vastus", image: "./pictures/sanasto/vastus.jpg"}
 ]
     
 let randomizeWord = ''
